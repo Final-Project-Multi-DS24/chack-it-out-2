e6cc87ed69d7a668ef85cd5a73198317c453a85e
Kim Sang-Hwi <bassstg@naver.com>
2023-07-18 13:56:25 +0900

    feat : 웹페이지 구현

--- web/book/admin.py
+++ web/book/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book
+
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("book_isbn", "book_title", "book_author")
+
+
+# User 앱 관리자에서 보여주기
+admin.site.register(Book, BookAdmin)
--- web/book/models.py
+++ web/book/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    book_isbn = models.IntegerField(unique=True, verbose_name="ISBN")
+    book_title = models.CharField(max_length=500, verbose_name="도서명")
+    book_author = models.CharField(max_length=20, verbose_name="저자")
+    book_publisher = models.CharField(max_length=20, verbose_name="출판사")
+    book_page = models.IntegerField(verbose_name="페이지 수")
+    book_description = models.CharField(max_length=1000, verbose_name="책소개")
+    book_cover = models.CharField(max_length=400, verbose_name="책표지")
+
+    #
+    def __str__(self):
+        return self.book_title
+
+    # db_table의 이름을 "tb_user로 설정"
+    class Meta:
+        db_table = "tb_book"
